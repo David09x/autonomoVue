e1008d412e6eca2c53d11c12ff2eeb0a967769b4
David09x <71710020+David09x@users.noreply.github.com>
2024-04-07 21:32:44 +0200

    Add files via upload
    vue Con enseñarCitas y obtener los ingresos y los gastos

---
+++ vue/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+//import obtenerCitas from './components/obtenerCitas.vue';
+import dinerito from './components/gastosYingresos.vue'
+</script>
+
+<template>
+ 
+  <dinerito />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
